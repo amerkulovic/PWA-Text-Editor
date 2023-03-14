04af8a0e66a6e3213373e1ec480ef798498ea299
Amer Kulovic <amer.kulovic@yahoo.com>
2023-03-13 21:38:58 -0500

    edited database.js code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,11 +14,11 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log("Put to the base");
+  console.log("Put to the database");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.add({ id: 1, content: content });
+  const request = store.add({ id: 1, value: content });
   const result = await request;
   console.log("🚀 - data saved to the database", result);
 };
